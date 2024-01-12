565371932265d01d8029e41afaf506a7506d62bc
Jose M. Gomez <gomezjosem@live.com>
2024-01-12 00:33:57 -0500

    develop microservices

---
+++ consumer/consumer.py
@@ -0,0 +1,36 @@
+# consumer.py
+import socket
+import cowsay
+import random
+import re
+
+LOCAL_HOST = '0.0.0.0'
+LOCAL_PORT = 12345
+
+# set random seed
+random.seed(42)
+
+# pick random character
+char = random.choice(cowsay.char_names)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((LOCAL_HOST, LOCAL_PORT))
+    s.listen()
+
+    print(f"\nConsumer is listening on port {LOCAL_PORT} (⊙_⊙👂)\n")
+
+    conn, addr = s.accept()
+    with conn:
+        print(f"(￣y▽￣)╭ Ohohoho..... Connected to {addr}💕 \n")
+        data = conn.recv(1024).decode()
+        print(f"\nConsumer received: \n{data}\n")
+
+        # Check if the received data has "Ping" (case insensitive)
+        if re.search("Ping", data, re.I) is not None:
+            response = cowsay.get_output_string(char, "Pong! 🏓").encode()
+            conn.sendall(response)
+            print(f"\nConsumer sent: \n{response.decode()}\n")
+        else:
+            response = cowsay.get_output_string(char, "no game 💔").encode()
+            conn.sendall(response)
+            print(f"\nConsumer sent: \n{response.decode()}\n")
---
+++ producer/producer.py
@@ -0,0 +1,27 @@
+# producer.py
+import socket
+import cowsay
+import random
+
+DESTINATION_HOST = 'consumer'
+DESTINATION_PORT = 12345
+
+# set random seed
+random.seed(24)
+
+# pick random character
+char = random.choice(cowsay.char_names)
+        
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    try:
+        s.connect((DESTINATION_HOST, DESTINATION_PORT))
+        user_input = input("\nIt's your turn to serve! 🏓\n\n")
+        data_to_send = cowsay.get_output_string(char, user_input).encode()
+        s.sendall(data_to_send)
+        print(f"\nProducer sent: \n{data_to_send.decode()}\n")
+
+        # Receive data from the consumer
+        data_received = s.recv(1024).decode()
+        print(f"\nProducer received: \n{data_received}\n")
+    except (BrokenPipeError, ConnectionResetError, ConnectionRefusedError):
+        print("\nConnection failed 💔\n")
